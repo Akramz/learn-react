27c1e66aa62825bc73bca480d6087ad16a6b21da
Akramz <MohamedAkramZaytar@gmail.com>
2018-10-13 17:50:53 +0100

    List logic to Method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,34 +4,28 @@ class Counter extends Component {
   // includes all of the data that the component needs.
   state = {
     count: 1,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please add Some Tags!"}
+        {this.renderTags()}
       </div>
     );
   }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-
   incrementCount() {
     this.setState({
       count: this.state.count + 1
